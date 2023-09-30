74e01628b059694f7557318af5be1ffa265b736c
Seong-Hoon-Lim <hooney200@gmail.com>
2023-09-30 23:22:45 +0900

    #6 - 프로젝트 구조 변경
    src 디렉토리 아래로 이동
    server.js 파일 외에 다른 모든 파일도 모두 src 디렉토리에 생성

--- src/server.js
+++ src/server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 mongoose.connect(`mongodb+srv://hooney200:hooney1108@cluster0.155h4r3.mongodb.net/?retryWrites=true&w=majority`)
     .then(() => {
         console.log('mongodb connected');
@@ -16,6 +20,13 @@ mongoose.connect(`mongodb+srv://hooney200:hooney1108@cluster0.155h4r3.mongodb.ne
     })
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+app.get('/signup', (req, res) => {
+    res.render('signup');
+})
 
 const port = 4000;
 app.listen(port, () => {
