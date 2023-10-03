59a9f660433430494f31741ee127fcccb7718cc2
Seong-Hoon-Lim <hooney200@gmail.com>
2023-10-03 21:52:39 +0900

    #14 - 라우터 관련 코드 리팩토링 및 라우터 등록
    app.use('/', mainRouter)
    app.use('/auth', usersRouter)
    각 라우터 등록 후 모든 기능 정상 동작 확인

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const User = require('./models/users.model');
-const passport = require('passport');
 const cookieSession = require('cookie-session');
+const passport = require('passport');
 const config = require('config');
+
+const User = require('./models/users.model');
+
+const serverConfig = config.get('server');
+const port = serverConfig.port;
+
+const mainRouter = require("./routes/main.router");
+const usersRouter = require("./routes/users.router");
+
 require('dotenv').config()
 
 const app = express();
@@ -37,7 +45,6 @@ app.use(function (request, response, next) {
 app.use(passport.initialize());
 app.use(passport.session());
 require('./config/passport');
-const {checkAuthenticated, checkNotAuthenticated} = require("./middlewares/auth");
 
 //view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -52,64 +59,9 @@ mongoose.connect(process.env.MONGO_URI)
     })
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/', mainRouter);
+app.use('/auth', usersRouter);
 
-app.get('/', checkAuthenticated, (req, res) => {
-    res.render('index');
-})
-
-app.get('/signup', checkNotAuthenticated, (req, res) => {
-    res.render('signup');
-})
-app.post('/signup', async (req, res) => {
-    //user 객체 생성
-    const user = new User(req.body);
-    //user 컬렉션에 user 저장
-    try {
-        await user.save();
-        return res.status(200).json({
-            success: true
-        })
-    } catch (error) {
-        console.error(error);
-    }
-})
-
-app.get('/login', checkNotAuthenticated, (req, res) => {
-    res.render('login');
-})
-
-app.post('/login', (req, res, next) => {
-    console.log('로그인을 시도')
-    passport.authenticate("local", (err, user, info) => {
-        console.log('일치하는 회원을 찾는 중')
-        if (err) {
-            return next(err);
-        }
-        if (!user) {
-            return res.json({ msg: info });
-        }
-        req.logIn(user, function (err) {
-            if (err) { return next(err); }
-            res.redirect('/');
-        });
-    }) (req, res, next);
-})
-
-app.get('/auth/google', passport.authenticate('google'));
-app.get('/auth/google/callback', passport.authenticate('google', {
-    successReturnToOrRedirect: '/',
-    failureRedirect: '/login'
-}))
-
-app.post('/logout', (req, res, next) => {
-    req.logOut(function (err) {
-        if (err) { return next(err) }
-        res.redirect('/login');
-    })
-})
-
-const serverConfig = config.get('server');
-const port = serverConfig.port;
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
 });
